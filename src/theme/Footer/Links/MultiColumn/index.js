11f15d9d9da1c4b9e6a7c23598017e325817efd4
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2023-03-02 11:33:12 +0100

    finished ui

--- src/theme/Footer/Links/MultiColumn/index.js
+++ src/theme/Footer/Links/MultiColumn/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import LinkItem from '@theme/Footer/LinkItem';
-function ColumnLinkItem({item}) {
+import React from "react";
+import LinkItem from "@theme/Footer/LinkItem";
+import styles from "./styles.module.css";
+
+function ColumnLinkItem({ item }) {
   return item.html ? (
     <li
       className="footer__item"
       // Developer provided the HTML, so assume it's safe.
       // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{__html: item.html}}
+      dangerouslySetInnerHTML={{ __html: item.html }}
     />
   ) : (
     <li key={item.href ?? item.to} className="footer__item">
@@ -14,7 +16,7 @@ function ColumnLinkItem({item}) {
     </li>
   );
 }
-function Column({column}) {
+function Column({ column }) {
   return (
     <div className="col footer__col">
       <div className="footer__title">{column.title}</div>
@@ -26,12 +28,27 @@ function Column({column}) {
     </div>
   );
 }
-export default function FooterLinksMultiColumn({columns}) {
+export default function FooterLinksMultiColumn({ columns }) {
   return (
     <div className="row footer__links">
       {columns.map((column, i) => (
         <Column key={i} column={column} />
       ))}
+      {/* Subscribe */}
+      <SubscribeComp />
     </div>
   );
 }
+
+const SubscribeComp = () => {
+  return (
+    <div className={styles.container}>
+      <h3>Subscribe to our newsletter</h3>
+      <p>Join text Join ... </p>
+      <div className={styles.input_container}>
+        <input type="text" placeholder="Enter Your Email" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
+};
